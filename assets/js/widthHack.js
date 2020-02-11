d577f29164c7d08d6c0294d0ada1f5fff4153971
Zoko11 <d.towers2@ncl.ac.uk>
2020-02-11 11:37:08 +0000

    Changed JavaScript to affect the canvas as well

--- assets/js/widthHack.js
+++ assets/js/widthHack.js
@@ -2,7 +2,12 @@
 $(window).resize(function() {
 	$(function width() {
 		var div = $('#gameContainer');
+		var canvas = $('#canvas');
+		
 		var width = div.width();
-		div.css('height', ((width/16)*9));
+		canvas.css('width', width);
+		var height = (width/16)*9;
+		canvas.css('height', height)
+		div.css('height', height);
 	});
 });
