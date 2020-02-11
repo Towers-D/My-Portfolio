d1f8cc9979e8410aee0fbaf30e56639392a6123c
Zoko11 <d.towers2@ncl.ac.uk>
2020-02-11 11:10:45 +0000

    changed function to resize when window does

--- assets/js/widthHack.js
+++ assets/js/widthHack.js
@@ -1,5 +1,9 @@
-$(function() {
+$(function width() {
 	var div = $('#gameContainer');
 	var width = div.width();
 	div.css('height', ((width/16)*9));
+});
+
+$(window.resize(function() {
+	width();
 });
