a9ec83b9f06d8f6cc23d105058af6a14473aa619
Zoko11 <d.towers2@ncl.ac.uk>
2020-02-11 11:25:17 +0000

    Moved function call

--- assets/js/widthHack.js
+++ assets/js/widthHack.js
@@ -1,9 +1,8 @@
-$(function width() {
-	var div = $('#gameContainer');
-	var width = div.width();
-	div.css('height', ((width/16)*9));
-});
 
 $(window).resize(function() {
-	width();
+	$(function width() {
+		var div = $('#gameContainer');
+		var width = div.width();
+		div.css('height', ((width/16)*9));
+	});
 });
